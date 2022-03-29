b4129a459797d4871afc231d10448a8296a7d52b
Daniel Dakhno <dakhnod@gmail.com>
2022-03-29 11:55:43 +0200

    added memory initialization and checks

--- main.cpp
+++ main.cpp
@@ -36,6 +36,9 @@ void databaseTest()
     std::cout << "enter DB location: ";
     std::cin >> location;
     db::Database db;
+    for(int i = 0; i < DB_LIMIT; i++){
+        db.schilder[i] = NULL;
+    }
     db.nameOfLocation = location;
     bool insertSuccessful;
 
